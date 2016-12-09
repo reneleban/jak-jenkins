d92691060288dcbe7b281c0b6eb9f52eb4f1c0d6
René Leban <rleban@oev.de>
2016-12-09 09:52:09 +0100

    remove second job

--- services.groovy
+++ services.groovy
@@ -7,7 +7,6 @@ projects.add([name: 'board', port: 10000])
 projects.each {
   def projectName = 'jak-' + it.name
   def shortName = it.name
-  def infrastructureName = it.name + '-infrastructure'
   def port = it.port
   def gitUrl = 'https://github.com/reneleban/jak-services.git'
   def branch = 'develop'
@@ -30,15 +29,6 @@ projects.each {
       shell("docker build -t ${projectName} -f ${shortName}.docker .")
       shell("docker tag ${projectName} localhost:5000/${projectName}:latest")
       shell("docker push localhost:5000/${projectName}-:latest")
-    }
-
-    postBuildSteps {
-      queue(infrastructureName)
-    }
-  }
-
-  job(infrastructureName) {
-    steps {
       shell("docker stop ${projectName}")
       shell("docker rm ${projectName}")
       shell("docker run -d -v ${projectName}-storage:/application/data -p ${port}:${port} --restart=always --name ${projectName} localhost:5000/${projectName}:latest")
